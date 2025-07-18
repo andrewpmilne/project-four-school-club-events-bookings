71328a0fd5146721d1f9d15e976f1296cd76c713
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-07-18 16:44:41 +0100

    create http response in users app

--- school_clubs_events/urls.py
+++ school_clubs_events/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users.views import my_user
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', my_user, name='home'), 
 ]
