f267ae1e6f9717320b118fc461dd26a723cd0bac
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-07-25 14:19:12 +0100

    add login form and redirect after successful sign up to login

---
+++ user/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from user.models import User
+from django.contrib.auth.forms import AuthenticationForm
+
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    password_confirm = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'role']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError("Passwords do not match.")
+        return cleaned_data
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
+class EmailAuthenticationForm(AuthenticationForm):
+    username = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'autofocus': True}))
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import EmailAuthenticationForm
 
 app_name = 'user'
+
 
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('signup/', views.signup_view, name='signup'),
+    path('login/', auth_views.LoginView.as_view(
+        template_name='user/login.html',
+        authentication_form=EmailAuthenticationForm
+    ), name='login'),
 ]
