ba16a7d9dccc91f87efd958be430d6139b650e05
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-08-07 14:48:23 +0100

    add base.html

--- school_clubs_events/settings.py
+++ school_clubs_events/settings.py
@@ -72,7 +72,7 @@ ROOT_URLCONF = 'school_clubs_events.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
