f7942a471196acd946399e22d25203adc911ad42
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-08-10 22:01:40 +0100

    add edit and delete child functionality

--- child/urls.py
+++ child/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import create_child, view_children_cards
+from .views import create_child, view_children_cards, edit_child, delete_child
 
 app_name = 'child'
 
 urlpatterns = [
     path('create/', create_child, name='create_child'),
     path('view/', view_children_cards, name='view_children_cards'),
+    path('edit/<int:child_id>/', edit_child, name='edit_child'),
+    path('delete/<int:child_id>/', delete_child, name='delete_child'),
 ]
--- child/views.py
+++ child/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ChildForm
 from .models import Child
-from user.decorators import role_required
+from user.decorators import role_required   
 
 
 @login_required
@@ -26,6 +26,7 @@ def create_child(request):
 
 
 @login_required
+@role_required('parent')
 def view_children_cards(request):
     """
     Display all children belonging to the logged-in parent in a card-style layout.
@@ -38,3 +39,44 @@ def view_children_cards(request):
             'children': children,
         }
     )
+
+
+@login_required
+@role_required('parent')
+def edit_child(request, child_id):
+    """
+    View to edit details of a child belonging to the logged-in parent.
+    """
+    child = get_object_or_404(Child, id=child_id, parent=request.user)
+
+    if request.method == 'POST':
+        form = ChildForm(request.POST, instance=child)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"{child.first_name} {child.surname}'s details updated successfully!")
+            return redirect('child:view_children_cards')
+        else:
+            messages.error(request, "Please fix the errors below.")
+    else:
+        form = ChildForm(instance=child)
+
+    return render(request, 'child/edit_child.html', {
+        'form': form,
+        'child': child,
+    })
+
+
+@login_required
+@role_required('parent')
+def delete_child(request, child_id):
+    """
+    Delete a child belonging to the logged-in parent.
+    """
+    child = get_object_or_404(Child, id=child_id, parent=request.user)
+
+    if request.method == 'POST':
+        child.delete()
+        messages.success(request, f"{child.first_name} {child.surname} has been deleted.")
+        return redirect('child:view_children_cards')
+    
+    return render(request, 'child/delete_child_confirm.html', {'child': child})
