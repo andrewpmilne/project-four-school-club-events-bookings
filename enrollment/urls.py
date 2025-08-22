331e2f37b36a2286471e7fb6d9d528d901c4ad8a
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2025-08-22 14:45:39 +0100

    add link to click on club and go straight to enrollment form with club already selected

--- enrollment/urls.py
+++ enrollment/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import create_enrollment
+from .views import create_enrollment, create_enrollment_with_club
 
 app_name = 'enrollment'
 
 urlpatterns = [
     path('create/', create_enrollment, name='create_enrollment'),
+    path(
+        'create/<int:club_id>/',
+        create_enrollment_with_club,
+        name='create_enrollment_with_club'
+        ),
 ]
